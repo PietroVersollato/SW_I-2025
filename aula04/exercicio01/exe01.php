9058d8f0146d99c71a14197078704a9f8c5a88d5
PietroVersollato <161628491+PietroVersollato@users.noreply.github.com>
2025-03-07 16:49:44 -0300

    aula04

---
+++ aula04/exercicio01/exe01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>NOTAS</h1>
+    <form action="form01.php" method="get">
+        <p>Digite a primeira nota: <input type="number" name="nota1" id="" required></p>
+        <p>Digite a segunda nota: <input type="number" name="nota2" id="" required></p>
+        <p>Digite a terceira nota: <input type="number" name="nota3" id="" required></p>
+        <input type="submit" value="ENVIAR">
+    </form>
+</body>
+</html>
---
+++ aula04/exercicio01/form01.php
@@ -0,0 +1,8 @@
+<?php
+    $nota1 = $_GET =['nota1'];
+    $nota2 = $_GET =['nota2'];
+    $nota3 = $_GET =['nota3'];
+
+    $media = ($nota1+$nota2+$nota3)/3;
+    echo "Sua média é: $media";
+?>
