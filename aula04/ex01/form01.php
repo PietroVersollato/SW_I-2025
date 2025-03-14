d6e6f046844ee3c96fcb7d589646181f45a6742a
PietroVersollato <161628491+PietroVersollato@users.noreply.github.com>
2025-03-14 14:44:52 -0300

    Delete aula04 directory

--- aula04/ex01/form01.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $nota01 = $_GET['cxnota01'];
-    $nota02 = $_GET['cxnota02'];
-    $nota03 = $_GET['cxnota03'];
-
-    $media = ($nota01+$nota02+$nota03)/3;
-    echo "Sua mensagem é: $media";
-?>
--- aula04/ex03/form02.php
+++
@@ -1,6 +0,0 @@
-<?php
-$corEscolhida = $_POST['cor'];
-echo "<body style='background-color: $corEscolhida;'>";
-echo "<h2>Cor de fundo alterada para $corEscolhida</h2>";
-echo "</body>";
-?>
